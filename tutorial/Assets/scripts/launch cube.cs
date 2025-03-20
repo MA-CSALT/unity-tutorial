a4576a3138712505e1ae2f724c009c37390cd97c
ruiter <38767@ma-web.nl>
2025-03-20 14:16:22 +0100

    tutorial 0 and 1

---
+++ tutorial/Assets/scripts/launch cube.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class launchcube : MonoBehaviour
+{
+    Rigidbody m_Rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            //Apply a force to this Rigidbody in direction of this GameObjects up axis
+            m_Rigidbody.AddForce(transform.up * 30f);
+        }
+    }
+}
